b78f433f70c1e37da428bf7ead034de2b14ee41d
Paulo Ferreira_CONTR <C0655173@vale.com>
2023-08-22 10:28:53 -0300

    Removendo criptografia das senhas (para facilitar testes)

--- desafio-4devs/desafio-4devs/UseCasses/Users/Add/UsersAddCommand.cs
+++ desafio-4devs/desafio-4devs/UseCasses/Users/Add/UsersAddCommand.cs
@@ -7,19 +7,16 @@ namespace desafio_4devs.UseCasses.Users.Add
     {
         public required string Name { get; set; }
         public required string Email { get; set; }
-        // Password should be encrypted
         public required string Password { get; set; }
 
         public static implicit operator User(UsersAddCommand command)
         {
-            // Hash and salt the password using bcrypt before storing it
-            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(command.Password);
 
             return new User
             {
                 Name = command.Name,
                 Email = command.Email,
-                Password = hashedPassword,
+                Password = command.Password,
                 CreatedAt = DateTime.Now
             };
         }
