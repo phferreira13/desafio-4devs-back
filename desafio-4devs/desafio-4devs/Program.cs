d5b77433ae4b35b9966cb5823da6ef428a181d68
Paulo Ferreira_CONTR <C0655173@vale.com>
2023-08-23 00:32:05 -0300

    Implementando autenticação de usuários

--- desafio-4devs/desafio-4devs/Program.cs
+++ desafio-4devs/desafio-4devs/Program.cs
@@ -1,7 +1,12 @@
 using desafio_4devs.UseCasses.Users;
 using desafio_4devs_entity.Context;
 using desafio_4devs_entity.Ioc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,9 +22,39 @@ builder.Services.AddCors(options =>
             .AllowAnyMethod());
 });
 
+//Configure JWT
+//Configure AuthController with no authentication
+builder.Services.AddControllers().AddMvcOptions(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                     .RequireAuthenticatedUser()
+                     .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
+
+var key = Encoding.ASCII.GetBytes(builder.Configuration["JwtSettings:Secret"]);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 builder.Services.AddEntityConfiguration(builder.Configuration);
 
 
@@ -27,7 +62,6 @@ builder.Services.AddEntityConfiguration(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(c => c.RegisterServicesFromAssembly(typeof(UsersBaseHandler).Assembly));
-
 
 
 
@@ -53,6 +87,7 @@ app.UseRouting();
 
 app.UseCors(AllowLocalhost);
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
